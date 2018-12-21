bf47d622ece8113d5f48ad74966772bd72b5b94f
Waldemar Bira <biraw@ryanair.com>
2018-12-21 21:52:36 +0100

    Added function for parsing lines to problems

--- main.go
+++ main.go
@@ -7,15 +7,17 @@ import (
 	"os"
 )
 
+type problem struct {
+	question string
+	answer   string
+}
+
 func exit(message string) {
 	fmt.Println(message)
 	os.Exit(1)
 }
 
-func main() {
-	csvFileName := flag.String("csvFile", "problems.csv", "csvFile defines path to csv file with questions and answers for quiz")
-	flag.Parse()
-
+func csvReader(csvFileName *string) [][]string {
 	file, error := os.Open(*csvFileName)
 	if error != nil {
 		exit(fmt.Sprintf("Failed to open files %s", *csvFileName))
@@ -26,6 +28,24 @@ func main() {
 	if error != nil {
 		exit("Failed to parse a file")
 	}
+	return lines
+}
 
-	fmt.Println(lines)
+func mapLinesToProblems(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem{
+			question: line[0],
+			answer:   line[1],
+		}
+	}
+	return problems
+}
+
+func main() {
+	csvFileName := flag.String("csvFile", "problems.csv", "csvFile defines path to csv file with questions and answers for quiz")
+	flag.Parse()
+	lines := csvReader(csvFileName)
+	problems := mapLinesToProblems(lines)
+	fmt.Println(problems)
 }
